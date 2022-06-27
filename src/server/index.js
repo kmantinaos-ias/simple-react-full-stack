2dc20a1809188b3025c51846c3eb996abe93c512
kmantinaos-ias <107168216+kmantinaos-ias@users.noreply.github.com>
2022-06-27 15:12:50 -0400

    absolute paths generated dynamically

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const path = require('path')
 // import file writing utils
 const flUtils = require('./utils/fraudFileUtils')
 
@@ -10,9 +11,9 @@ function mapHeaderStrings (rawHeaderStrings) {
 	const data = {}
 	const keysToExclude = new Set(['host', 'referer'])
 
-	for (i=0; i<rawHeaderStrings.length; i+=2){
+	for (i=0; i<rawHeaderStrings.length; i+=2) {
 		let lowCasedKey = rawHeaderStrings[i].toLowerCase()
-		
+
 		if (!keysToExclude.has(lowCasedKey)) {
 			data[lowCasedKey] = rawHeaderStrings[i+1]
 		}
@@ -24,20 +25,20 @@ function mapHeaderStrings (rawHeaderStrings) {
 // run rules engine on input files
 async function passInputFileToRulesEngine () {
 
-	const inputPath = '/Users/kmantinaos/Documents/GitHub/simple-react-full-stack/src/server/temp/input.csv'
-	const outputPath = '/Users/kmantinaos/Documents/GitHub/simple-react-full-stack/src/server/temp/engineOutput.json'
-
+	const inputPath = path.resolve(__dirname, './temp/input.csv')
+	const outputPath = path.resolve(__dirname, './temp/engineOutput.json')
+	
 	return new Promise(function (resolve, reject) {
 		var exec = require('child_process').exec;
 
 		//syntax and how to use rules engine cli: https://confluence.integralads.com/pages/viewpage.action?spaceKey=EN&title=Running+Rules+Engine+CLI
 
-		var javaCommandStr = 'java -cp rules-engine-2.jar com.beehive.analytics.App ' + 
-		'-inputFileName ' + inputPath + 
-		' -outputFileName ' + outputPath + 
+		var javaCommandStr = 'java -cp rules-engine-2.jar com.beehive.analytics.App ' +
+		'-inputFileName ' + inputPath +
+		' -outputFileName ' + outputPath +
 		' -parseUserAgent true'
 
-		exec(javaCommandStr, {cwd: '/Users/kmantinaos/Documents/GitHub/simple-react-full-stack/src/server'}, function (err, a, b) {
+		exec(javaCommandStr, {cwd: './src/server'}, function (err, a, b) {
 			if (err) {
 				console.log('error passInputFileToRulesEngine()', err);
 				reject();
@@ -80,7 +81,7 @@ app.get('/api/profile_browser', async (req, res) => {
 	res.send({ username: `you are browsing on ${name.slice(0, name.length-5)}`})
 });
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`))
 
 /*
 Chrome: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36
